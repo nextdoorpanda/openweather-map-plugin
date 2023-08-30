4f8bc2528f22582765a05900c8194e213ddeaa3e
Konstantinos Megas <nextdoorpanda@gmail.com>
2023-08-30 12:47:50 +0300

    add getData()

--- assets/js/ci-openweather.js
+++ assets/js/ci-openweather.js
@@ -1,11 +1,13 @@
-function getData() {
-    const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+// function getData() {
+//     const requestOptions = {
+//         method: 'GET',
+//         redirect: 'follow'
+//     };
+//
+//     fetch("http://api.openweathermap.org/data/2.5/forecast?q=Athens,GR&limit=1&appid=063f1e01c653468ba9b0d5726b9cee53", requestOptions)
+//         .then(response => response.text())
+//         .then(result => console.log(result))
+//         .catch(error => console.log('error', error));
+// }
 
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q=Athens,GR&limit=1&appid=063f1e01c653468ba9b0d5726b9cee53", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-}
+window.alert('test');
