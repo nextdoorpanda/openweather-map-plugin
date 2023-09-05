5882245d92c590af4cb584931f2558901a2d669a
Konstantinos Megas <nextdoorpanda@gmail.com>
2023-09-05 18:05:04 +0300

    update fetch function

--- assets/js/ci-openweather.js
+++ assets/js/ci-openweather.js
@@ -1,6 +1,5 @@
-(function getData() {
-
-    let weatherContent = document.querySelector('#weather-output').innerHTML;
+(function () {
+    'use strict';
 
     const requestOptions = {
         method: 'GET',
@@ -24,9 +23,28 @@
 
     const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=Athens,GR&limit=1&appid=063f1e01c653468ba9b0d5726b9cee53`;
 
-    fetch(queryURL, requestOptions)
-        .then(response => response.json())
-        .then(data => weatherContent += JSON.stringify(data))
-        .catch(error => console.log('error', error));
+    const getData = new Promise((resolve, reject) => {
+        fetch(queryURL, requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Didn't got any response");
+                }
+                return response.json();
+            })
+            .then(data => {
+                resolve(data);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
 
+    getData
+        .then(weatherData => {
+            const outputDiv = document.querySelector('#weather-output');
+            outputDiv.innerHTML = JSON.stringify(weatherData);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 })();
