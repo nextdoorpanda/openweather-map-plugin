b9a46702e58594f82869fb5072c298da5e7b830c
Konstantinos Megas <nextdoorpanda@gmail.com>
2023-09-06 11:14:27 +0300

    limit number of fetched results

--- assets/js/ci-openweather.js
+++ assets/js/ci-openweather.js
@@ -21,7 +21,7 @@
     //     })
     //     .catch(error => console.log('error', error));
 
-    const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=Athens,GR&limit=1&appid=063f1e01c653468ba9b0d5726b9cee53`;
+    const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=Athens,GR&cnt=1&appid=063f1e01c653468ba9b0d5726b9cee53`;
 
     const getData = new Promise((resolve, reject) => {
         fetch(queryURL, requestOptions)
