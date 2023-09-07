078b4f14cad8c1cf09825604d3e52f67cd1f94d7
Konstantinos Megas <nextdoorpanda@gmail.com>
2023-09-07 14:44:49 +0300

    remove commented out code

--- assets/js/ci-openweather.js
+++ assets/js/ci-openweather.js
@@ -6,21 +6,6 @@
         redirect: 'follow'
     };
 
-    // fetch("./key.txt")
-    //     .then(response => response.text())
-    //     .then(apiKey => {
-    //         apiKey = apiKey.trim();
-    //         console.log(apiKey);
-    //         const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=Athens,GR&limit=1&appid=${apiKey}`;
-    //
-    //         // fetch(queryURL, requestOptions)
-    //         //     .then(response => response.json())
-    //         //     .then(data => console.log(data))
-    //         //     .catch(error => console.log('error', error));
-    //
-    //     })
-    //     .catch(error => console.log('error', error));
-
     const queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=Athens,GR&cnt=1&appid=063f1e01c653468ba9b0d5726b9cee53`;
 
     const getData = new Promise((resolve, reject) => {
