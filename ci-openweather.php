03f31b15f03139d95743cf28519d587f5b9a360f
Konstantinos Megas <nextdoorpanda@gmail.com>
2023-09-11 23:32:44 +0300

    fix phpcs errors & warnings

--- ci-openweather.php
+++ ci-openweather.php
@@ -29,17 +29,19 @@ function ciopenweather_enqueue_scripts()
 	wp_register_script( 'ci-openweather-js', CIOPENWEATHER_URL . '/assets/js/ci-openweather.js', array(), false, true );
 
 
-	$api_key = get_option('ci-openweather_api_key');
-	$location = get_option('ci-openweather_location');
-	$unit = get_option('ci-openweather_unit');
+	$api_key  = get_option( 'ci-openweather_api_key' );
+	$location = get_option( 'ci-openweather_location' );
+	$unit     = get_option( 'ci-openweather_unit' );
 
-	// Pass DB values to JS script
-	wp_localize_script('ci-openweather-js', 'weatherOptionsValues', array(
-		'api_key' => $api_key,
+	// Pass DB values to JS script.
+	wp_localize_script( 'ci-openweather-js', 'weatherOptionsValues', array(
+		'api_key'  => $api_key,
 		'location' => $location,
-		'unit' => $unit,));
+		'unit'     => $unit,
+	)
+	);
 
-	wp_enqueue_script('ci-openweather-js');
+	wp_enqueue_script( 'ci-openweather-js' );
 }
 add_action( 'wp_enqueue_scripts', 'ciopenweather_enqueue_scripts' );
 
@@ -57,7 +59,7 @@ function ciopenweather_settings_markup() {
 	?>
 
 	<div class="wrap">
-		<h2><?php _e( get_admin_page_title(), 'ci-openweather' ) ?></h2>
+		<h2><?php esc_html_e( 'OpenWeather Map', 'ci-openweather' ); ?></h2>
 		<form action="options.php" method="post">
 			<?php settings_fields( 'ci-openweather' ); ?>
 			<?php do_settings_sections( 'ci-openweather' ); ?>
@@ -70,8 +72,8 @@ function ciopenweather_settings_markup() {
 }
 
 	add_action( 'admin_init', 'ciopenweather_admin_init' );
-	function ciopenweather_admin_init(){
-		// Create Settings
+	function ciopenweather_admin_init() {
+		// Create Settings.
 		$ciopenweather_option_group = 'ci-openweather';
 
 		$ciopenweather_api_key_option = 'ci-openweather_api_key';
@@ -83,45 +85,46 @@ function ciopenweather_settings_markup() {
 		$ciopenweather_unit_option = 'ci-openweather_unit';
 		register_setting( $ciopenweather_option_group, $ciopenweather_unit_option );
 
-		// Create section of Page
+		// Create section of Page.
 		$ciopenweather_settings_section = 'ci-openweather_main_section';
+
 		$ciopenweather_page = 'ci-openweather';
 		add_settings_section( $ciopenweather_settings_section, __( 'Settings', 'ci-openweather' ), 'ciopenweather_main_section_text_output', $ciopenweather_page );
 
-		// Add fields to section
-		add_settings_field( $ciopenweather_api_key_option, __('OpenWeather API Key', 'ci-openweather' ), 'ciopenweather_api_key_option_input', $ciopenweather_page, $ciopenweather_settings_section );
+		// Add fields to section.
+		add_settings_field( $ciopenweather_api_key_option, __( 'OpenWeather API Key', 'ci-openweather' ), 'ciopenweather_api_key_option_input', $ciopenweather_page, $ciopenweather_settings_section );
 
-		add_settings_field($ciopenweather_location_option, __('Your Location (e.g. "Athens, GR")', 'ci-openweather'), 'ciopenweather_location_option_input', $ciopenweather_page, $ciopenweather_settings_section );
+		add_settings_field( $ciopenweather_location_option, __( 'Your Location (e.g. "Athens, GR")', 'ci-openweather' ), 'ciopenweather_location_option_input', $ciopenweather_page, $ciopenweather_settings_section );
 
-		add_settings_field($ciopenweather_unit_option, __('Unit', 'ci-openweather'), 'ciopenweather_unit_option_input', $ciopenweather_page, $ciopenweather_settings_section );
+		add_settings_field( $ciopenweather_unit_option, __( 'Unit', 'ci-openweather' ), 'ciopenweather_unit_option_input', $ciopenweather_page, $ciopenweather_settings_section );
 	}
 
 function ciopenweather_main_section_text_output() {
-	_e( '<p>Please enter your preferred settings.</p>', 'ci-openweather' );
+	echo '<p>' . esc_html_e( 'Please enter your preferred settings.', 'ci-openweather' ) . '</p>';
 }
 
 function ciopenweather_api_key_option_input() {
 	$api_key = get_option( 'ci-openweather_api_key' );
 	echo '<input id="openweather-api_key" name="ci-openweather_api_key" value="' . esc_attr( $api_key ) . '" type="text" autocomplete="off" class="widefat" required>';
-	echo "<p>" . wp_kses( __( 'Enter your <strong>API key</strong>.', 'ci-openweather' ), array( 'strong' => array() ) ) . "</p>";
+	echo '<p>' . wp_kses( __( 'Enter your <strong>API key</strong>.', 'ci-openweather' ), array( 'strong' => array() ) ) . '</p>';
 }
 
 function ciopenweather_location_option_input() {
 	$location = get_option( 'ci-openweather_location' );
 	echo '<input id="openweather-location" name="ci-openweather_location" value="' . esc_attr( $location ) . '" type="text" autocomplete="off" class="widefat" required>';
-	echo "<p>" . wp_kses( __( 'Enter your <strong>location</strong>.', 'ci-openweather' ), array( 'strong' => array() ) ) . "</p>";
+	echo '<p>' . wp_kses( __( 'Enter your <strong>location</strong>.', 'ci-openweather' ), array( 'strong' => array() ) ) . '</p>';
 }
 
 function ciopenweather_unit_option_input() {
 	$unit = get_option( 'ci-openweather_unit' );
 	?>
 	<select name="ci-openweather_unit">
-		<option value="imperial" <?php selected(esc_attr( $unit ), 'imperial'); ?>>Imperial (&deg;F)</option>
-		<option value="metric" <?php selected(esc_attr( $unit ), 'metric'); ?>>Celsius (&deg;C)</option>
-		<option value="standard" <?php selected(esc_attr( $unit ), 'standard'); ?>>Kelvin (K)</option>
+		<option value="imperial" <?php selected( esc_attr( $unit ), 'imperial' ); ?>>Imperial (&deg;F)</option>
+		<option value="metric" <?php selected( esc_attr( $unit ), 'metric' ); ?>>Celsius (&deg;C)</option>
+		<option value="standard" <?php selected( esc_attr( $unit ), 'standard' ); ?>>Kelvin (K)</option>
 	</select>
 	<?php
-	echo "<p>" . wp_kses( __( 'Enter the <strong>unit of measurement</strong>.', 'ci-openweather' ), array( 'strong' => array() ) ) . "</p>";
+	echo '<p>' . wp_kses( __( 'Enter the <strong>unit of measurement</strong>.', 'ci-openweather' ), array( 'strong' => array() ) ) . '</p>';
 }
 
 
@@ -142,6 +145,8 @@ add_shortcode( 'ci-openweather', 'shortcode_ciopenweather' );
 
 function shortcode_ciopenweather( $atts = [], $content ) {
 	$weather_output = '<div class="weather-output"></div>';
+
 	$content .= $weather_output;
+
 	return $content;
 }
